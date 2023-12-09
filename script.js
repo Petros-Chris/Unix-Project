326f6ef51c8f216171b20b241653ac09f18f5076
chrischiken <2271356@edu.vaniercollege.qc.ca>
2023-12-08 19:16:24 -0500

    test 26

--- script.js
+++ script.js
@@ -1,18 +1,28 @@
 
-runCommand();
- function runCommand() {
-    const { exec } = require('child_process');
+export function runCommand() {
+    const serverUrl = '167.99.185.29'; // Replace with your server URL
     const command = 'mcrcon -H 174.88.85.18 -p password "deop WomanFunny"';
 
-    // Example: execute the 'ls' command
-    exec(command, (error) => {
-        if (error) {
-            console.error(`Error: ${error.message}`);
-            return;
-        }
+    // Sending a POST request to the server
+    fetch(`${serverUrl}/execute-command`, {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({ command }),
+    })
+    .then(response => response.json())
+    .then(data => {
+        console.log('Server response:', data);
+        // Perform any actions based on the server response
+        fetchLog();
+        document.getElementById("userInput").value = "";
+    })
+    .catch(error => {
+        console.error('Error:', error);
     });
-
-    //location.reload(); //and this would reload the page, loading the log again
-    fetchLog(); //or you could just run the fetch log thing again
-    document.getElementById("userInput").value = ""; //this would enpty the text
 }
+
+
+
+const command = 'mcrcon -H 174.88.85.18 -p password "deop WomanFunny"';
